ac42af342075def30ca557d99e207e9813d18f85
marlon-frazao <frazao.marlon@gmail.com>
2021-05-13 11:03:22 -0300

    MovieService

---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,36 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exception.ResourceNotFoundException;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+	
+	@Autowired
+	private GenreRepository genreRepository;
+	
+	@Transactional
+	public Page<MovieDTO> findAllPaged(Long genreId, PageRequest pageRequest) {
+		Genre genre = (genreId == 0) ? null : genreRepository.getOne(genreId);
+		Page<Movie> page = repository.find(genre, pageRequest);
+		return page.map(Movie::toDto);
+	}
+	
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found")).toDto();
+	}
+}
